a54211d7c39a3d18ccc457cc8209d86cf569dbe8
Om Yadav <omydv3@gmail.com>
2024-09-23 13:24:14 +0530

    initalize folder

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <p className='text-white text-5xl'>
+        Portfolio Website
+      </p>
+    </div>
+  )
+}
+
+export default App

fe5f5eca37dab62110a4517f77229d868558ae57
OmYadav3 <omydv3@gmail.com>
2025-06-03 12:26:57 +0530

    Add new Project FivePages

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -18,7 +18,7 @@ const Project = () => {
                 <div className="project-info p-4">
                   <h2 className="text-2xl lg:text-4xl font-bold">Recipe Page</h2>
                   <p className="text-gray-600 mb-2 text-xs lg:text-lg">It's a recipe page of Simple Omelette . Mentions their Ingredients , Instructions to make and Nutrients table. </p>
-                  <a href="https://omyadav-notification-page.netlify.app/" className=" text-xs lg:text-lg text-white hover:underline border-2 p-2 bg-red-500 rounded-md">
+                  <a href="https://omyadav-Recipe-page.netlify.app/" className=" text-xs lg:text-lg text-white hover:underline border-2 p-2 bg-red-500 rounded-md">
                   Preview
                   </a>
                   <a href="https://github.com/OmYadav3/frontend_mentor_projects/tree/main/Recipe-page" className="text-xs lg:text-lg text-white ml-4 border-2 p-2 bg-black rounded-md ">
@@ -79,6 +79,25 @@ const Project = () => {
                     Preview
                   </a>
                   <a href="https://github.com/OmYadav3/youtube-backend" className="text-xs lg:text-lg text-white ml-4 border-2 p-2 bg-black rounded-md ">
+                    Github 
+                  </a>
+                </div>
+            </div>
+            <div className="border border-black  project-card mt-2 rounded bg-white shadow-xl">
+                <div className='w-full overflow-hidden '>
+                <img
+                  className="w-full  "
+                  src="Fivepages.png"
+                  alt="project"
+                />
+                </div>                
+                <div className="project-info p-4">
+                  <h2 className="text-2xl lg:text-4xl font-bold">FivePages</h2>
+                  <p className="text-gray-600 mb-2 text-xs lg:text-lg">It is a novel webite and It is a complete Full Stack project with all the features a backend project should have.</p>
+                  <a href="https://five-pages-delta.vercel.app/" className="text-xs lg:text-lg text-white hover:underline border-2 p-2 bg-red-500 rounded-md">
+                    Preview
+                  </a>
+                  <a href="https://github.com/OmYadav3/fivePages" className="text-xs lg:text-lg text-white ml-4 border-2 p-2 bg-black rounded-md ">
                     Github 
                   </a>
                 </div>
